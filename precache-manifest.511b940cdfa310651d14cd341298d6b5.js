bca617b47a4b4cbd702cc0e163ba7d9d6fef23ca
mmusil2 <mmusil2@kent.edu>
2019-03-18 21:02:31 -0400

    Updates

---
+++ precache-manifest.511b940cdfa310651d14cd341298d6b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "5716c5f15c43d75cc059",
+    "url": "/ClickyGame/static/js/main.0c2063c4.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/ClickyGame/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5716c5f15c43d75cc059",
+    "url": "/ClickyGame/static/css/main.298e80d2.chunk.css"
+  },
+  {
+    "revision": "e92ca4b83b942660aa152ca3a40fc8d0",
+    "url": "/ClickyGame/index.html"
+  }
+];
